916c53147d1b206eb7c79c6f41e8d4f348b66bf0
wellingtonmont22 <wellington.m.alves@hotmail.com>
2021-01-30 22:40:19 -0300

    Cria comida

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direct = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = 'lightgreen';
@@ -19,7 +23,10 @@ function criarCobrinha() {
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
-
+function foodDrop() {
+    context.fillStyle = "white";
+    context.fillRect(food.x, food.y, box, box);
+}
 document.addEventListener('keydown', update);
 
 function update(event) {
@@ -37,6 +44,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    foodDrop();
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
