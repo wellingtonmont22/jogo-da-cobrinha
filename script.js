efb0a9549bebd0b9ca27d0393092697ac325b80c
wellingtonmont22 <wellington.m.alves@hotmail.com>
2021-01-30 22:33:26 -0300

    direção e atravessar paredes

--- script.js
+++ script.js
@@ -19,7 +19,22 @@ function criarCobrinha() {
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if(event.keyCode == 37 && direct != "right") direct = "left";
+    if(event.keyCode == 38 && direct != "down") direct = "up";
+    if(event.keyCode == 39 && direct != "left") direct = "right";
+    if(event.keyCode == 40 && direct != "up") direct = "down";
+}
+
 function iniciarJogo() {
+    if(snake[0].x > 15 * box && direct == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direct == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direct == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direct == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
     let snakeX = snake[0].x;
@@ -27,8 +42,8 @@ function iniciarJogo() {
 
     if(direct == "right") snakeX += box;
     if(direct == "left") snakeX -= box;
-    if (direct == "up") snakeY += box;
-    if (direct == "down") snakeY -= box;
+    if (direct == "up") snakeY -= box;
+    if (direct == "down") snakeY += box;
 
     snake.pop();
 
