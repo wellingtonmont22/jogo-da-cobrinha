e79e1bb97d802480f2ae947f069ba503f2aafd89
wellingtonmont22 <wellington.m.alves@hotmail.com>
2021-01-30 22:55:16 -0300

    morte da cobrinha

--- script.js
+++ script.js
@@ -42,6 +42,13 @@ function iniciarJogo() {
     if(snake[0].y > 15 * box && direct == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direct == "up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert("Fim de Jogo");
+        }
+    }
+
     criarBG();
     criarCobrinha();
     foodDrop();
